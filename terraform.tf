c1c42f73de2b6143abeb46dc2bd58cef0b8793e0
Amit Roy <amitkumar.roy@petronas.com>
2023-08-29 20:44:00 +0530

    fmt

--- terraform.tf
+++ terraform.tf
@@ -1,16 +1,16 @@
 terraform {
-    required_providers {
-        azurerm = {
-            source = "hashicorp/azurerm"
-           version = ">= 3.7.0"
-        }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.7.0"
     }
-    backend "azurerm" {
-        resource_group_name  = "amit-rg"
-        storage_account_name = "amitstgtf"
-        container_name       = "tfstate"
-        key = "terraform.tfstate"
-    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "amit-rg"
+    storage_account_name = "amitstgtf"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
