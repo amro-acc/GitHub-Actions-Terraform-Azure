44482a9c43760ab70aade77f1a6e344260f5a0a2
Amit Roy <amitkumar.roy@petronas.com>
2023-08-29 20:58:36 +0530

    change variables

--- variables.tf
+++ variables.tf
@@ -1,11 +1,2 @@
-variable "resource_group_name" {
-  description = "Name of the Azure Resource Group"
-  type        = string
-  default     = "amit-actions-rg"
-}
-
-variable "location" {
-  description = "Location for the Azure Resource Group"
-  type        = string
-  default     = "East US"
-}
+variable "resource_group_name" {}
+variable "location" {}
