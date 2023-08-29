d615d5c1814371e66ced929b7aa32782e74d2261
Amit Roy <amitkumar.roy@petronas.com>
2023-08-29 19:38:35 +0530

    add tf and modify pipeline

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  description = "Name of the Azure Resource Group"
+  type        = string
+  default     = "amit-actions-rg"
+}
+
+variable "resource_group_location" {
+  description = "Location for the Azure Resource Group"
+  type        = string
+  default     = "East US"
+}
