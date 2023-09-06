9cc13ba55f81d5907e976371f852fc89b0e5b8e7
Amit Roy <126342793+amro-acc@users.noreply.github.com>
2023-09-06 20:35:54 +0530

    version

--- provider.tf
+++ provider.tf
@@ -5,7 +5,7 @@ terraform {
       version = "3.71.0"
     }
     databricks = {
-      source = "databricks/databricks"
+      source = "databrickslabs/databricks"
       version = "1.5.0"
     }
   }
